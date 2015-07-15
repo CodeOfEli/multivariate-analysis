dd2c52a9ad753b4c6e90e80ae99bd71d97142c86
ugp <ugp@ugps-MacBook-3.local>
2015-07-15 18:27:18 -0500

    added csv data

--- multivariate.py
+++ multivariate.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import statsmodels.api as sm
 
+# Downloaded from https://www.lendingclub.com/info/download-data.action
+lendingClub = pd.read_csv('LoanStats3a.csv')
 
-lendingClub = pd.read_csv('https://www.lendingclub.com/info/download-data.action')
-
-#print lendingClub
+print lendingClub.head()
